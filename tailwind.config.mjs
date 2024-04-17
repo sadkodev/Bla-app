8b0d6decee61a257678e18bce1341a5e9759cdbc
sadkodev <kristiankastillo0@gmail.com>
2024-04-16 22:57:37 -0400

    ci(app): Colors tailwind configs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,30 @@
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#5B4B8D',
+                secondary: '#B58A62',
+                tertiary: '#D9A566',
+                status: {
+                    success: '#4CAF50',
+                    warning: '#FFC107',
+                    danger: '#F44336',
+                },
+                shadow: {
+                    light: 'rgba(0, 0, 0, 0.05)',
+                    dark: 'rgba(0, 0, 0, 0.1)',
+                },
+                bg_color: {
+                    light: '#F9FAFB',
+                    dark: '#10151D',
+                },
+                fg_color: {
+                    light: '#2D3748',
+                    dark: '#BFC7D2',
+                },
+            },
+        },
     },
     plugins: [],
 }
