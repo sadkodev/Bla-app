a368b0f2c5a5a1fd9e234e8809e10bbf498fd474
sadkodev <kristiankastillo0@gmail.com>
2024-04-19 12:29:07 -0400

    ci(app): support for vervel and database

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import react from '@astrojs/react'
-import { defineConfig } from 'astro/config'
+import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel/serverless";
 
-import tailwind from '@astrojs/tailwind'
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind()],
-})
+  integrations: [react(), tailwind(), db()],
+  output: "server",
+  adapter: vercel()
+});
