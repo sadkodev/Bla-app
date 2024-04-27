c428f689c6aac2f50db589405cee1db316dafb29
Ignacio Diaz <informatica.diaz@gmail.com>
2024-04-26 23:13:02 -0300

    docs(utils): Document the  file using JSDoc format
    Commented variables  and  to explain their purpose and type¸
    -Documented the  function within the  configuration.
    -Extended the  module to add custom types using .
    -Defined the  interface to represent user attributes in the database.

--- src/auth.ts
+++ src/auth.ts
@@ -3,8 +3,16 @@ import { Session, User, db } from 'astro:db'
 import { Lucia } from 'lucia'
 
 // Change the type of db in future versions of Astro
+/**
+ * Adaptador SQLite para el sistema de autenticación Lucía.
+ * @type {DrizzleSQLiteAdapter}
+ */
 const adapter = new DrizzleSQLiteAdapter(db as any, Session, User)
 
+/**
+ * Intancia principal de la aplicación Lucía.
+ * @type {Lucia}
+*/
 export const lucia = new Lucia(adapter, {
     sessionCookie: {
         attributes: {
@@ -13,6 +21,11 @@ export const lucia = new Lucia(adapter, {
         },
     },
 
+    /**
+     * Función para obtener los atributos del usuario.
+     * @param {DatabaseUserAttributes} attributes - Atributos del usuario en la base de datos.
+     * @returns {object} - Atributos del usario para la sesión.
+     */
     getUserAttributes: (attributes) => {
         return {
             // attributes has the type of DatabaseUserAttributes
@@ -21,13 +34,33 @@ export const lucia = new Lucia(adapter, {
     },
 })
 
+/**
+ * Extensión de módulo para añadir tipos personalizados a Lucía.
+ * @module lucia
+ */
 declare module 'lucia' {
     interface Register {
+        /**
+         * Clase principal de la aplicación Lucía.
+         * @type {typeof lucia}
+         */
         Lucia: typeof lucia
+        /**
+         * Interfaz para los atributos de usario en la base de datos
+         * @type {DatabaseUserAttributes}
+         */
         DatabaseUserAttributes: DatabaseUserAttributes
     }
 }
 
+/**
+ * Interfaz para los atributos de usuario en la base de datos
+ * @interface DatabaseUserAttributes
+ */
 interface DatabaseUserAttributes {
+    /**
+     * Nombre de usuario del usuario en la base de datos
+     * @type {string}
+     */
     username: string
 }
