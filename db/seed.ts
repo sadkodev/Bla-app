a1db23727eb395d42c8af0d1c7428ad00ec03389
Ignacio Diaz <informatica.diaz@gmail.com>
2024-04-28 17:48:25 -0300

    docs(api): Documented db/seed.ts using JSDoc format
    I provide information about the seed function, its purpose, and how it's used.

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,9 @@
+/**
+ * Función asíncrona para sembrar la base de datos con datos iniciales.
+ * @returns {Promise<void>} 
+ * Una promesa que se resuelve cuando se completa el proceso de simbra.
+ */
 // https://astro.build/db/seed
 export default async function seed() {
-    // TODO
+    // TODO: Agregar lógica para sembrar la base de datos con datos iniciales.
 }
