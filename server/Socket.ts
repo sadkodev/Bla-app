5fc8a6951ab9137ed6f4fbb3b95b8324a8f9d145
Tsurematsu <daniel120802@gmail.com>
2024-05-02 13:06:55 -0500

    Actualization  Socket io

--- server/Socket.ts
+++ server/Socket.ts
@@ -1,34 +1,17 @@
-import http from 'http'
-import { Server } from 'socket.io'
-type Server_type = http.Server<
-  typeof http.IncomingMessage,
-  typeof http.ServerResponse
->
-export default function Socket(server: Server_type): Server {
-  const io = new Server(server)
-  const ClientCache: any = {}
-  io.on('connection', (socket) => {
-    ClientCache[socket.id] = ClientCache[socket.id] ?? {}
-    socket.on('join', async (room, ...args) => {
-      ClientCache[socket.id].room = ClientCache[socket.id].room ?? room
-      await socket.join(room)
-      io.to(room).emit('join', String(socket.id))
+import { Server } from "socket.io";
+import http from "http";
+type Server_type = http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
+export default function Socket(server:Server_type): any{
+  const io = new Server(server);
+  io.on("connection", (socket) => {
+    socket.on("join", (room)=>{
+      socket.join(room);
+      socket.to(room).emit("join", socket.id);
     })
-    socket.on(
-      'message',
-      async (event = null, data = null, addressee = null) => {
-        ClientCache[socket.id] = ClientCache[socket.id] ?? {}
-        ClientCache[socket.id].data = ClientCache[socket.id].data ?? data
-        const roomClient = ClientCache[socket.id].room
-        const EmtSocket = roomClient ? socket.to(roomClient) : socket.broadcast
-        EmtSocket.emit(
-          event ?? 'message',
-          data ?? [],
-          socket.id, //sender
-          addressee //addressee
-        )
-      }
-    )
-  })
-  return io
+    socket.on("message", ({event="message", data={}, to=null})=>{
+      let sendSocket = to!==null?io.sockets.sockets.get(to)??socket.broadcast:socket.broadcast; 
+      sendSocket.emit(event, data, socket.id);
+    })
+  });
+  return io;
 }
