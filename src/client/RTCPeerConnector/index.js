3906ada6bd6540c9245d964665f904bba9adb6a1
Tsurematsu <daniel120802@gmail.com>
2024-05-03 22:41:22 -0500

    refactory

--- src/client/RTCPeerConnector/index.js
+++ src/client/RTCPeerConnector/index.js
@@ -1,33 +1,21 @@
-async function ReadJSON(URL) {
-    const response = await fetch(URL);
-    if (!response.ok) {throw new Error(`HTTP error! status: ${response.status} =>  ${URL}`);}
-    return await response.json();
-}
-
-export default function RTCPeerConnector({
-            iceServers="./RTCPeerConnector/iceServers.json", 
-            PathIO="./socket.io.esm.min.js", 
-            UrlSocket="/",
-            room="default"
-        }) {
+import iceServers from "./iceServers.js";
+export default function RTCPeerConnector(socket) {
     const eventNames = {
         offer: "offer",
         answer: "answer",
         candidate: "candidate",
         join: "join",
     }
-    let IceServers = null;
     this.peerClients = {};
-    this.LocalEvents = {
+    this.events = {
         connect:()=>{},
         join:(client)=>{},
         leave:(id)=>{},
         configPeer:(peerClient, client)=>{},
-        configClose:()=>{},
     };
     const createPeerClient = (client, socket) => {
-        const peerClient = new RTCPeerConnection({iceServers:IceServers})
-        this.LocalEvents.configPeer(peerClient, client);
+        const peerClient = new RTCPeerConnection({iceServers})
+        this.events.configPeer(peerClient, client);
         peerClient.onicecandidate = (event) => {if (event.candidate) {
             socket.send({
                 event:eventNames.candidate, 
@@ -37,36 +25,30 @@ export default function RTCPeerConnector({
         }};
         return peerClient;
     };
-
-    this.socket = null;
+    
     this.start = async ()=>{
-        IceServers = await ReadJSON(iceServers);
-        if (this.socket === null) {
-            const io = (await import(PathIO)).default;
-            this.socket = io(UrlSocket);
-        }
-        this.socket.on("connect", () => {
-            this.LocalEvents.connect();
-            this.socket.emit(eventNames.join, room);
+        socket.on("connect", () => {
+            this.events.connect();
+            socket.emit(eventNames.join, room);
         });
-        this.socket.on(eventNames.join, (client) => {
-            this.LocalEvents.join(client);
-            this.peerClients[client] = createPeerClient(client, this.socket);
+        socket.on(eventNames.join, (client) => {
+            this.events.join(client);
+            this.peerClients[client] = createPeerClient(client, socket);
             this.peerClients[client].createOffer().then((offer) => {
                 this.peerClients[client].setLocalDescription(offer);
-                this.socket.send({
+                socket.send({
                     event:eventNames.offer, 
                     data:offer, 
                     to:client
                 });
             });
         });
-        this.socket.on(eventNames.offer, (offer, sender) => {
-            this.peerClients[sender] = createPeerClient(sender, this.socket);
+        socket.on(eventNames.offer, (offer, sender) => {
+            this.peerClients[sender] = createPeerClient(sender, socket);
             this.peerClients[sender].setRemoteDescription(new RTCSessionDescription(offer));
             this.peerClients[sender].createAnswer().then((answer) => {
                 this.peerClients[sender].setLocalDescription(answer);
-                this.socket.send({
+                socket.send({
                     event:eventNames.answer, 
                     data:answer, 
                     to:sender
@@ -74,24 +56,17 @@ export default function RTCPeerConnector({
             });
         });
 
-        this.socket.on(eventNames.answer, (answer, sender) => {
+        socket.on(eventNames.answer, (answer, sender) => {
             this.peerClients[sender].setRemoteDescription(new RTCSessionDescription(answer));
         });
 
-        this.socket.on(eventNames.candidate, (candidate, sender) => {
+        socket.on(eventNames.candidate, (candidate, sender) => {
             this.peerClients[sender].addIceCandidate(new RTCIceCandidate(candidate));
         });
-
-        this.socket.on("leave", (id) => {
-            this.LocalEvents.leave(id);
+        socket.on("leave", (id) => {
+            this.events.leave(id);
             this.peerClients[id].close();
             delete this.peerClients[id];
         });
-        
-        return this.socket;
     }
-    this.close = ()=>{
-        this.LocalEvents.configClose();
-        this.socket.close();
-    };
 }
